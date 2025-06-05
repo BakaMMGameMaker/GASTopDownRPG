0f48816873fb709907d0867017e251e801bf6b26
Fuxuanmylove <2793275118@qq.com>
2025-06-05 23:06:08 +0800

    Create own RPGPlayerState Class to get ready for adding GAS in Player Class

---
+++ Source/GASTopDownRPG/Private/Player/RPGPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright ZJU doMGameMaker.
+
+
+#include "Player/RPGPlayerState.h"
+
+ARPGPlayerState::ARPGPlayerState()
+{
+	SetNetUpdateFrequency(100.f);
+}
---
+++ Source/GASTopDownRPG/Public/Player/RPGPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright ZJU doMGameMaker.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "RPGPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASTOPDOWNRPG_API ARPGPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	ARPGPlayerState();
+};
