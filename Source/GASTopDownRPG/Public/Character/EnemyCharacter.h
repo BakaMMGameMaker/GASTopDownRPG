4bbedd95efcb07f29ae465819b17f34eb0205143
Fuxuanmylove <2793275118@qq.com>
2025-06-05 16:43:21 +0800

    Create EnemyInterface to get ready for highlighting actors hovered by cursor

--- Source/GASTopDownRPG/Public/Character/EnemyCharacter.h
+++ Source/GASTopDownRPG/Public/Character/EnemyCharacter.h
@@ -4,14 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "Character/RPGCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "EnemyCharacter.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class GASTOPDOWNRPG_API AEnemyCharacter : public ARPGCharacterBase
+class GASTOPDOWNRPG_API AEnemyCharacter : public ARPGCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 };
