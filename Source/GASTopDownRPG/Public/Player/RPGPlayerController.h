8ea4e231d5d2fa4793a668222a221cf351df07df
Fuxuanmylove <2793275118@qq.com>
2025-06-05 14:49:32 +0800

    Add new class for RPGPlayerController

---
+++ Source/GASTopDownRPG/Public/Player/RPGPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright ZJU doMGameMaker.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RPGPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASTOPDOWNRPG_API ARPGPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
