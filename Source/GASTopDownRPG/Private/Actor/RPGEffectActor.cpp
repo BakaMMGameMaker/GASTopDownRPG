de17b4c4dd1cad8538d894690423be2dee16ddff
Fuxuanmylove <2793275118@qq.com>
2025-06-06 16:29:37 +0800

    Test the accessors for attributes in attribute set in OnOverlap Function for EffectActor's Sphere

---
+++ Source/GASTopDownRPG/Private/Actor/RPGEffectActor.cpp
@@ -0,0 +1,46 @@
+// Copyright ZJU doMGameMaker.
+
+
+#include "Actor/RPGEffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/RPGAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+ARPGEffectActor::ARPGEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
+	
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(GetRootComponent());
+}
+
+void ARPGEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//todo: Change this to apply a GamePlay Effect
+	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const URPGAttributeSet* RPGAttributeSet = Cast<URPGAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(URPGAttributeSet::StaticClass()));
+		URPGAttributeSet* MutableRPGAttributeSet = const_cast<URPGAttributeSet*>(RPGAttributeSet);
+		MutableRPGAttributeSet->SetHealth(RPGAttributeSet->GetHealth() + 25.f);
+		Destroy();
+	}
+}
+
+void ARPGEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+void ARPGEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ARPGEffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &ARPGEffectActor::EndOverlap);
+}
