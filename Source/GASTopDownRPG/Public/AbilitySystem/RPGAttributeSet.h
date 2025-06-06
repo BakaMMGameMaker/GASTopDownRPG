7b8f60a5ff723b67d6d42245c8131035ce969cdf
Fuxuanmylove <2793275118@qq.com>
2025-06-06 14:06:24 +0800

    Setup 4 new attributes for the gameplay ability system

--- Source/GASTopDownRPG/Public/AbilitySystem/RPGAttributeSet.h
+++ Source/GASTopDownRPG/Public/AbilitySystem/RPGAttributeSet.h
@@ -13,5 +13,35 @@ UCLASS()
 class GASTOPDOWNRPG_API URPGAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	URPGAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+
+	// If pass in an argument which can be replicated, the function will receive the old value
+	// so you can compare the old value and the new one in the function
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 	
 };
